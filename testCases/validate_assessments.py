8532db0d81c048308a6db1fe5b256446f9b0ade4
Pardhasaradhi-edwisely <pardhasaradhi@edwisely.com>
2022-06-15 16:20:11 +0530

    modified tcs

--- testCases/validate_assessments.py
+++ testCases/validate_assessments.py
@@ -8,28 +8,71 @@ from utilities.readData import ReadConfig
 from utilities.customLogger import LogGen
 from utilities import XLUtils
 
+
 class Test_002_assessment:
     logger = LogGen.loggen()
     path = ".\\TestData\\testdata.xlsx"
 
     @pytest.mark.regression
-    def test_validateAssessmentPage(self,setup):
-        self.logger.info("***Test_002_assessment***")
-        self.logger.info("***Validate Assessment Page***")
+    def test_validateAssessmentTitle(self,setup):
+        self.logger.info("**** Test_002_assessment ****")
+        self.logger.info("**** TC_01 Validate Assessment Page ****")
+        self.driver = setup
+        self.url=XLUtils.readData(self.path,'Sheet1', 2 ,1)
+        print("page url",self.url)
+        self.driver.get(self.url)
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(10)
+        self.page_title=self.driver.title
+        print(self.page_title)
+        self.status = XLUtils.dataValidation(self.path,'Sheet1', self.page_title)
+        print(self.status)
+        self.log = LogGen.statusValidation(self,self.status, self.page_title, self.driver)
+        self.logger.info(self.log)
+        self.li = login(self.driver)
+        self.username=XLUtils.readData(self.path,'Sheet1',2,2)
+        print("username:",self.username)
+        self.li.facultyUsername(self.username)
+        self.password=XLUtils.readData(self.path,'Sheet1',2,3)
+        print("Password: *******")
+        self.li.facultyPassword(self.password)
+        self.li.submit_02()
+        time.sleep(3)
+        self.dashboard_title = self.driver.title
+        print(self.dashboard_title)
+        self.status = XLUtils.dataValidation(self.path, 'Sheet1', self.dashboard_title)
+        print(self.status)
+        self.log = LogGen.statusValidation(self, self.status, self.dashboard_title, self.driver)
+        self.logger.info(self.log)
+        self.asses=assessment(self.driver)
+        self.asses.clickAssessment()
+        self.driver.implicitly_wait(10)
+        self.assesTitle=self.driver.title
+        print(self.assesTitle)
+        self.status=XLUtils.dataValidation(self.path,'Sheet1',self.assesTitle)
+        print(self.status)
+        self.log = LogGen.statusValidation(self, self.status, self.assesTitle, self.driver)
+        self.logger.info(self.log)
+
+
+    @pytest.mark.sanity
+    def test_CreateObjectiveAssessment(self,setup):
+        self.logger.info("**** Test_002_Assessment ****")
+        self.logger.info("**** TC_02 Validate Create Objective Assessment ****")
         self.driver = setup
         self.url=XLUtils.readData(self.path,'Sheet1', 2 ,1)
         self.driver.get(self.url)
         self.driver.maximize_window()
         self.driver.implicitly_wait(10)
         self.page_title=self.driver.title
-        print(self.page_title)
-        self.expected_title=XLUtils.readData(self.path,'Sheet1',2,4)
-        if self.page_title==self.expected_title:
-            self.logger.info("***Title Matched***")
+        self.status = XLUtils.dataValidation(self.path, 'Sheet1', self.page_title)
+        if self.status == "True":
             assert True
         else:
-            self.logger.info("***Title UnMatched***")
+            self.logger.info("Page Title Not Matched")
+            self.driver.save_screenshot(".//Screenshots//" + "pageTitle.png")
             assert False
+            self.driver.close()
         self.li = login(self.driver)
         self.username=XLUtils.readData(self.path,'Sheet1',2,2)
         self.li.facultyUsername(self.username)
@@ -37,17 +80,13 @@ class Test_002_assessment:
         self.li.facultyPassword(self.password)
         self.li.submit_02()
         time.sleep(3)
-        self.page_title = self.driver.title
-        print(self.page_title)
-        self.exp_login_page_title=XLUtils.readData(self.path,'Sheet1',2,5)
-        if self.page_title == self.exp_login_page_title:
-            print("LOGIN SUCCESSFUL ")
-            self.logger.info("*****Login Successful****")
+        self.dashboard_title = self.driver.title
+        self.status = XLUtils.dataValidation(self.path, 'Sheet1', self.dashboard_title)
+        if self.status == "True":
             assert True
         else:
             self.driver.save_screenshot(".//Screenshots//" + "test_facultyLogin.png")
-            print('lOGIN UNSUCCESSFUL')
-            self.logger.error("******Login Unsuccessful")
+            self.logger.error("******Login Unsuccessful***")
             assert False
             self.driver.close()
         self.asses=assessment(self.driver)
@@ -55,58 +94,15 @@ class Test_002_assessment:
         self.driver.implicitly_wait(10)
         self.assesTitle=self.driver.title
         print(self.assesTitle)
-        self.row=XLUtils.getRowCount(self.path,'Sheet1')
-        print("row count",self.row)
-        self.col=XLUtils.getColumnCount(self.path,'Sheet1')
-        print("column count", self.col)
-        for r in range(1,self.row+1):
-            for c in range(1,self.col+1):
-                if self.assesTitle==XLUtils.readData(self.path,'Sheet1',r,c):
-                    self.logger.info("**Assessment title page matched**")
-                    assert True
-
-
-    @pytest.mark.sanity
-    def test_CreateObjectiveAssessment(self,setup):
-        self.logger.info("***Test_002_assessment***")
-        self.logger.info("***validate create objective assessment***")
-        self.driver = setup
-        self.url=XLUtils.readData(self.path,'Sheet1', 2 ,1)
-        self.driver.get(self.url)
-        self.driver.implicitly_wait(10)
-        self.page_title=self.driver.title
-        self.expected_title=XLUtils.readData(self.path,'Sheet1',2,4)
-        if self.page_title==self.expected_title:
+        self.status = XLUtils.dataValidation(self.path, 'Sheet1', self.assesTitle)
+        print(self.status)
+        if self.status == "True":
             assert True
         else:
             assert False
             self.driver.close()
-        self.li = login(self.driver)
-        self.username=XLUtils.readData(self.path,'Sheet1',2,2)
-        self.li.facultyUsername(self.username)
-        self.password=XLUtils.readData(self.path,'Sheet1',2,3)
-        self.li.facultyPassword(self.password)
-        self.li.submit_02()
-        time.sleep(3)
-        self.page_title = self.driver.title
-        self.exp_login_page_title=XLUtils.readData(self.path,'Sheet1',2,5)
-        if self.page_title == self.exp_login_page_title:
-            assert True
-        else:
-            self.driver.save_screenshot(".//Screenshots//" + "loginpage.png")
-            assert False
-            self.driver.close()
-        self.asses=assessment(self.driver)
-        self.asses.clickAssessment()
-        self.driver.implicitly_wait(10)
-        self.assesTitle=self.driver.title
-        self.exp=XLUtils.readData(self.path,"Sheet1",2,6)
-        if self.assesTitle==self.exp:
-            assert True
-        else:
-            assert False
-            self.driver.close()
-            self.driver.save_screenshot(".//Screenshots//" + "assessmentTitle.png")
+            self.screen=random_generator() + "image.png"
+            self.driver.save_screenshot(".//Screenshots//" + self.screen)
         time.sleep(2)
         self.asses.clickCreateNewAssessment()
         self.asses.clickObjectiveAssessment()
@@ -125,7 +121,20 @@ class Test_002_assessment:
                     self.logger.info("successfully navigated to create new assessment page")
                     assert True
 
-
+    @pytest.mark.sanity
+    def test_validateAssessment(self,setup):
+        self.driver = setup
+        self.url=XLUtils.readData(self.path,'Sheet1', 2 ,1)
+        print("page url",self.url)
+        self.driver.get(self.url)
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(10)
+        self.page_title=self.driver.title
+        print(self.page_title)
+        self.status = XLUtils.dataValidation(self.path, 'Sheet1', self.page_title)
+        print(self.status)
+        self.log=LogGen.statusValidation(self,self.status,self.page_title,self.driver)
+        self.logger.info(self.log)
 
 
 
